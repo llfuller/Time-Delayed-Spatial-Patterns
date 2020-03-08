d7a09d2cded83af9cb90613b37ccf66b0a337c1f
Lawson Fuller <llfuller@users.noreply.github.com>
2020-03-07 22:41:29 -0800

    Still needs spiral behavior and quicker runtime
    Periodic boundary conditions applied. Miscalculation in r dot e calculation fixed.
    
    Current issues:
    - Loading r_0 must be reviewed (causing problems with loading W).
    - Runs slowly in general. Use broadcasting for arrays in dfs.d_theta_d_t. Also simplify periodic boundary condition distance calculations.
    - N=1 still must be removed from main.py to allow N to be set correctly.
    - Spirals not yet achievable, but something like a traveling roll is.
    - Change num_video_loops to 100 (or use while loop) and interpolation to 'bicubic' for cooler visuals.

--- definitions.py
+++ definitions.py
@@ -1,36 +1,57 @@
 import scipy as sp
+import time
 
 """
 Calculates distance between two points (k,l) and (i, j) (named r_kl_ij).
 """
-def calc_r(k,l,i,j):
+def calc_r(k,l,i,j,num_rows,num_cols,horizontal_dist,vertical_dist,h_dist_1, h_dist_2, v_dist_1, v_dist_2):
     # some content
-    r_klij = sp.sqrt((i-k)**2 + (j-l)**2)
+    r11 = sp.sqrt((h_dist_1)**2 + (v_dist_1)**2)
+    r12 = sp.sqrt((h_dist_1)**2 + (v_dist_2)**2)
+    r21 = sp.sqrt((h_dist_2)**2 + (v_dist_1)**2)
+    r22 = sp.sqrt((h_dist_2)**2 + (v_dist_2)**2)
+    r_klij = min([r11,r12,r21,r22])
+    # r_klij = sp.sqrt((horizontal_dist)**2 + (vertical_dist)**2)
     return r_klij
+
 
 """
 Calculates W(r_klij).
 """
-def W(r_klij):
-    W_klij = 1.0/r_klij
-    return W_klij
+def calc_W(r):
+    W = sp.divide(1.0,r)
+    return W
 
 """
-Calculates a full (i,j,k,l) matrix of W which is identical except for
-zeros where r_klij > r_0
+Fills out W and dist_grid arrays
 """
-def calc_W_limited(W, r_0, num_rows, num_cols):
-    W_limited = sp.zeros((num_rows,num_cols,num_rows,num_cols))
+def calc_and_save_W_and_dist_grid(num_rows, num_cols, r_0):
+    W = sp.zeros((num_rows, num_cols, num_rows, num_cols))
+    dist_grid_arr = -1 * sp.ones((num_rows, num_cols, num_rows, num_cols))
+    start_time = time.time()
     for i in range(num_rows):
+        print("Row "+str(i)+" at time "+str(time.time()-start_time))
         for j in range(num_cols):
             for k in range(num_rows):
+                h_dist_1 = abs(i-k)
+                h_dist_2 = num_rows - abs(i-k)
+                horizontal_dist = float(h_dist_1*(h_dist_1<h_dist_2) + h_dist_2*(h_dist_2<h_dist_1))
                 for l in range(num_cols):
-                    if calc_r(k,l,i,j) > r_0:
-                        W_limited[i,j,k,l] = 0
-                    else:
-                        W_limited[i,j,k,l] = W[i,j,k,l]
-    return W_limited
-
+                    v_dist_1 = abs(j - l)
+                    v_dist_2 = num_cols - abs(j - l)
+                    vertical_dist = float(v_dist_1 * (v_dist_1 < v_dist_2) + v_dist_2 * (v_dist_2 < v_dist_1))
+                    r_klij = calc_r(i,j,k,l,num_rows, num_cols,horizontal_dist, vertical_dist, h_dist_1, h_dist_2, v_dist_1, v_dist_2)
+                    if r_klij<0 or horizontal_dist<0 or vertical_dist<0:
+                        print("something is wrong here")
+                    if r_klij<r_0:
+                        dist_grid_arr[i,j,k,l] = r_klij
+                        if r_klij != 0: # to avoid infinities
+                            W[i,j,k,l] = calc_W(r_klij)
+                        else: # This should probably be zero so that it doesn't affect any calculations.
+                            W[i, j, k, l] = 0
+    sp.savez("W_matrix_"+str((num_rows,num_cols,r_0))+".npz",W=W)
+    print("W matrix saved.")
+    return W, dist_grid_arr
 
 """
 Calculates sum of weights.
@@ -43,17 +64,17 @@ def N(W_limited, num_cols, num_rows):
 """
 Phi matrix
 """
-def phi(gamma, unit_vector_e, phi_0, num_rows, num_cols):
-    r = sp.array([[(i,j) for j in range(num_cols)] for i in range(num_rows)])
-    cos_of_angle_between = sp.divide(r[0]*unit_vector_e[0]+r[1]*unit_vector_e[1], sp.sqrt(r[0]**2 + r[1]**2))
-    # 1 x 50x50x2 + 50x50 = 50x2 + 50x50
-    phi = sp.multiply(gamma , cos_of_angle_between) + phi_0
+def calc_phi(gamma, unit_vector_e, phi_0, r):
+    epsilon = 0.000000000000000001 # present to avoid errors when dividing by zero for r = 0
+    r_times_cos_of_angle_between = (r*unit_vector_e[0])[:,:,0]+(r*unit_vector_e[1])[:,:,1]
+    print("Shape of r times cosine of angle between: "+str(sp.shape(r_times_cos_of_angle_between)))
+    phi = sp.multiply(gamma , r_times_cos_of_angle_between) + phi_0
     return phi
 
 """
 Theta matrix evaluated at specific time  
 """
-def theta(Omega, t, phi):
+def calc_theta(Omega, t, phi):
     # phi is (num_rows, num_cols) dimensional.
     theta = Omega*t+phi
     return theta
@@ -61,17 +82,21 @@ def theta(Omega, t, phi):
 """
 Accepts (i,j) positions and evaluates (then returns) d(theta)/dt matrix (elements are (i,j) component)
 """
-def d_theta_d_t(w, K, N, t, v, Omega, num_rows, num_cols):
-    sum_sub_term = sp.zeros((num_rows, num_cols, num_rows, num_cols))# array with indices (i,j,k,l); W*sin() part of sum
-    sum_term = sp.zeros((num_rows,num_cols))
-    for i in range(num_rows):
-        for j in range(num_cols):
-            for k in range(num_rows):
-                for l in range(num_cols):
-                    theta_kl_delayed = theta(Omega, t-float(calc_r(k,l,i,j))/v, phi[k,l])
-                    theta_ij_at_t = theta(Omega, t, phi[i,j])
-                    sum_sub_term[i,j,k,l] = sp.multiply(W(calc_r(i,j,k,l)), sp.sin(theta_kl_delayed -  theta_ij_at_t))
-            sum_term[i,j] = sp.sum(sum_sub_term, axes=(2,3))
+def d_theta_d_t(theta, t, phi, w, K, N, v, Omega, num_rows, num_cols, W_matrix, dist_grid_arr):
+    sum_sub_term = sp.zeros((num_rows, num_cols))# array with indices (i,j,k,l); W*sin() part of sum
+    sum_term = 0
+    print("Simulating for t="+str(t))
+    # Should be 2d
+    theta_ij_matrix_at_t = sp.array(theta).reshape((num_rows,num_cols)) # restores theta to initial shape for matrix algebra purposes
+    phi_extend = sp.array([[phi for k in range(num_rows)] for l in range(num_cols)]).transpose((2,3,1,0))
+    theta_ij_matrix_at_t_extend = sp.array([[theta_ij_matrix_at_t for k in range(num_rows)] for l in range(num_cols)]).transpose((2,3,1,0))
+    # Should be 4d
+    theta_kl_delayed = calc_theta(Omega, t-dist_grid_arr[:,:,:,:]/v, phi_extend[:,:])
+    # Should be 2d
+    sum_sub_term = sp.multiply(W_matrix, sp.sin(theta_kl_delayed -  theta_ij_matrix_at_t_extend)) # (k,l)
+    # Should be 2d
+    sum_term = sp.sum(sum_sub_term, axis = (2,3))
+
     dthetadt_final = w + sp.multiply(sp.divide(float(K),N) , sum_term) # (array with indices (i,j))
     return dthetadt_final
 
@@ -79,7 +104,7 @@ def d_theta_d_t(w, K, N, t, v, Omega, num_rows, num_cols):
 """
 System of ODEs to be solved
 """
-def theODEs(theta, t, w, K, N, v, Omega, num_rows, num_cols):
+def theODEs(theta, t, phi, w, K, N, v, Omega, num_rows, num_cols, W_matrix, dist_grid_arr):
     # t should be a time here, I think
-    dthetadt = [d_theta_d_t(w, K, N, t, v, Omega, num_rows, num_cols)]
-    return dthetadt
+    dthetadt = d_theta_d_t(theta, t, phi, w, K, N, v, Omega, num_rows, num_cols, W_matrix, dist_grid_arr)
+    return dthetadt.reshape(num_cols*num_rows)
--- main.py
+++ main.py
@@ -1,42 +1,103 @@
 import scipy as sp
 import matplotlib as plt
+import matplotlib.pyplot as pyplt
 import definitions as dfs
 from scipy.integrate import odeint
+import time
+from os import path
 
 # Purpose: Reproduce results of Time-Delayed Spatial Patterns in a Two-Dimensional Array of Coupled Oscillators
 #           by Seong-Ok Jeong, Tae-Wook Ko, and Hie-Tae Moon of Korea Advanced Institute of Science and Technology.
 # Project for Phys 278
-# Feb 27, 2020
+# March 5, 2020
+#######################################################################
+### Parameters and Initial Conditions
+#######################################################################
+sp.random.seed(2020)
 
-num_rows = 50 # 500 x 500 too big
-num_cols = 50
-grid_arr = sp.array([[(i,j) for j in range(num_cols)] for i in range(num_rows)])
+num_rows = 50  # 500 x 500 too big
+num_cols = 50  # 150 x 150 gives a 4 GB file
+                # 100 x 100 gives a 800 MB file
+
+grid_arr = sp.array([[[i,j] for j in range(num_cols)] for i in range(num_rows)])
 
 # k and i identify rows, l and j identify columns
-w = sp.pi / 5 # used in paper
-K = 1 # varies, see fig 4 
-v = 1
-#t = None
-gamma = 5 * np.pi / 32 # one of values used in paper
-unit_vector_e = sp.array([1,0])
+w = 7.0*sp.pi / 32 # used in paper
+K = 1.25 # varies, see fig 4
+v = 1.0/1
+Omega = 0.6*10
+gamma = 5 * sp.pi / 32 # one of values used in paper
+unit_vector_e = sp.array([1,1])
 phi_0 = sp.zeros((num_rows, num_cols))
-r_0 = 4 # coupling radius, experiment with this 
-initial_conditions = []
+r_0 = 15 # coupling radius, experiment with this
+initial_conditions = sp.random.random((num_rows, num_cols)).reshape(num_rows*num_cols) # Flattens initial conditions
+numTimeSteps = 5
+t = sp.linspace(0,1,numTimeSteps)
 
-W = sp.empty((num_rows, num_cols, num_rows, num_cols))
+parameters = [num_rows, num_cols, w, K, v, Omega, gamma, unit_vector_e, r_0, initial_conditions,numTimeSteps, t]
+
+#######################################################################
+### Calculate or load W matrix
+#######################################################################
+W = sp.zeros((num_rows, num_cols, num_rows, num_cols))
 N = sp.empty((num_rows, num_cols))
-# Calculate W_klij for all ij and kl. SO SLOW. 
-for i in range(num_rows):
-    for j in range(num_cols):
-        for k in range(num_rows):
-            for l in range(num_cols):
-                r_klij = sp.sqrt((i-k)**2+(j-l)**2)
-                W[i,j,k,l] = dfs.W(r_klij)
+dist_grid_arr = -1*sp.ones((num_rows, num_cols, num_rows, num_cols)) # i, j, k, l
+# Calculate W_klij for all ij and kl.
+start_time = time.time()
 
-W_limited = dfs.calc_W_limited(W, r_0, num_rows, num_cols)
-N = dfs.N(W_limited, num_cols, num_rows)
-phi  = dfs.phi(gamma, unit_vector_e, phi_0, num_rows, num_cols)
+# Make sure to delete old W matrix file if r_0 is changed.
+loaded_r_0 = r_0 # default value
+if path.exists("r_0.npz"):
+    print("Loading previous r_0")
+    loaded_r_0 = sp.load("r_0.npz")["r_0"]
+    if loaded_r_0 != r_0:
+        print("Using new r_0. Saving old r_0.")
+        sp.savez("r_0_previous.npz", r_0=loaded_r_0)
+    else:
+        print("Using current r_0 (same as previous r_0).")
+else:
+    print("Previous r_0 not available. Using new r_0.")
+    sp.savez("r_0.npz", r_0=r_0)
 
-t = sp.linspace(0,10,101)
-solution = odeint(dfs.theODEs, initial_conditions, t)
 
+if path.exists("W_matrix_"+str((num_rows,num_cols,r_0))+".npz"): # If file exists
+    W_loaded = sp.load("W_matrix_"+str((num_rows,num_cols,r_0))+".npz")["W"]
+    if (sp.shape(W_loaded)[0]!=num_rows or sp.shape(W_loaded)[1]!=num_cols or r_0 != loaded_r_0): # if dimensions of loaded array not same as specified
+        print("Loaded W matrix doesn't meet specified dimensions. Saving old W file and making new one.")
+        sp.savez("W_matrix_previous.npz", W = W_loaded)
+        W, dist_grid_arr = dfs.calc_and_save_W_and_dist_grid(num_rows, num_cols, r_0)
+    if (sp.shape(W_loaded)[0]==num_rows and sp.shape(W_loaded)[1]==num_cols and (r_0 == loaded_r_0)):
+        # if dimensions of loaded array are same as specified
+        W = W_loaded
+        print("W matrix loaded.")
+else: # If file does not yet exist
+    W, dist_grid_arr = dfs.calc_and_save_W_and_dist_grid(num_rows, num_cols, r_0)
+
+#######################################################################
+### Rest of the Calculations
+#######################################################################
+N = dfs.N(W, num_cols, num_rows)
+N = 1
+phi  = dfs.calc_phi(gamma, unit_vector_e, phi_0, grid_arr)
+print("Calculating solution for all selected times.")
+solution = odeint(dfs.theODEs, initial_conditions, t, args=(phi, w, K, N, v, Omega, num_rows, num_cols, W,
+                                                            dist_grid_arr))
+sp.savez("solution_("+str(num_cols)+","+str(num_rows)+")_r0_"+str(r_0), solution = solution, parameters = parameters, dist_grid_arr = dist_grid_arr)
+#######################################################################
+### Plot Solution
+#######################################################################
+print("Plotting solution")
+solution_2d = solution.reshape((numTimeSteps,num_rows,num_cols))
+print(sp.shape(solution))
+pyplt.title("Solution at Final Time")
+pyplt.imshow(solution_2d[numTimeSteps-1])
+pyplt.show()
+
+fig, ax = pyplt.subplots()
+num_video_loops = 10
+for j in range(num_video_loops):
+    for i in range(len(solution_2d)):
+        ax.cla()
+        ax.imshow(solution_2d[i], cmap = 'hsv', interpolation = 'nearest')
+        ax.set_title("Solution at frame {}".format(i))
+        pyplt.pause(0.1)
