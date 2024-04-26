3d8c25acf2c6318e534e2c28d467847f016825ac
Lawson Fuller <llfuller@users.noreply.github.com>
2024-04-26 01:04:20 -0700

    Adding final results
    (March 2020)

---
+++ DDE.m
@@ -0,0 +1,86 @@
+function sol = Oscillators2D
+% Using DDE by The MathWorks, Inc.
+% See DDE tutorial pdf.
+addpath('npy-matlab')
+addpath('generated_values')
+% Import values
+W = single(readNPY(['generated_values/W.npy']));
+%dist_grid_arr = readNPY(['generated_values/dist_grid_arr.npy']);
+%phi = readNPY(['generated_values/phi.npy']);
+N = single(readNPY(['generated_values/N.npy']));
+initial_conditions = single(readNPY(['generated_values/initial_conditions.npy']));
+scalars = readNPY(['generated_values/scalars.npy']);
+lag_times_reduced = single(readNPY(['generated_values/lags_reduced.npy']));
+lag_indices = single(readNPY(['generated_values/lag_indices.npy']));
+w = single(scalars(1));
+K = single(scalars(2));
+%v = scalars(3);
+%Omega = scalars(4);
+%gamma = scalars(5);
+num_rows = scalars(6);
+num_cols = scalars(7);
+%numTimeSteps = scalars(8);
+size_of_lag_matrix = size(lag_times_reduced);
+num_lag_values = size_of_lag_matrix(1);
+
+theta_lag_ijkl = single(zeros(num_rows,num_cols,num_rows,num_cols));
+
+options = ddeset('RelTol',1e-6,'AbsTol',1e-11);
+W_4_dim = W;
+
+lag_indices(isnan(lag_indices))=1;
+
+% Calculate solution over time
+% thrid argument: [0, 150] is timespan for run
+theta_observed_by_ij_at_kl_temp = single(zeros(num_rows*num_cols, num_rows, num_cols));
+tic
+sol = dde23(@RHS_of_ODE,lag_times_reduced,initial_conditions,[0, 5000],...
+    options,N,w,K,num_rows,num_cols,lag_indices,num_lag_values, W_4_dim, ...
+    theta_observed_by_ij_at_kl_temp);
+toc
+size_sol = size(sol.y)
+num_timesteps = size_sol(2)
+sol_matrix = mod(reshape(sol.y,num_rows,num_cols,num_timesteps),2*pi);
+save 'sol_matrix.mat' sol_matrix
+save 'sol_object.mat' sol
+% plot solution
+yint = deval(sol, linspace(0, 1000))
+size_yint = size(yint)
+plot_matrix_temp = reshape(yint,num_rows,num_cols,size_yint(2))
+plot_matrix = mod(plot_matrix_temp,2*pi);
+figure
+for t = 1:num_timesteps 
+    imagesc(plot_matrix(:,:,t))
+    colorbar()
+    colormap hsv
+    pause(0.1)
+end
+
+%-----------------------------------------------------------------------
+
+function y_final = RHS_of_ODE(t, y, Z, N, w, K, num_rows, ...
+    num_cols, lag_indices, num_lag_values, W_4_dim,...
+    theta_observed_by_ij_at_kl_temp)
+% y (actual theta values) are in column form. Reshape to (i,j) matrix form:
+theta_at_t = single(reshape(y,num_rows,num_cols));
+% change from (i,j) indexing to (i,j,k,l) indexing
+theta_ij_extend = repmat(theta_at_t,1,1,num_rows,num_cols);
+% Z (delayed theta values) are in column form. Reshape to indices (i,j,delay)
+theta_lagged_values = reshape(single(Z),num_rows, num_cols, num_lag_values);
+for k = 1:num_rows
+    for l = 1:num_cols
+        theta_observed_by_ij_at_kl_temp(:,k,l) = theta_lagged_values(k,l,lag_indices(:,:,k,l));
+    end
+end
+theta_observed_by_ij_at_kl = single(reshape(theta_observed_by_ij_at_kl_temp, num_rows, num_cols, num_rows, num_cols));
+
+% Calculate subterms inside sum with indices (i,j,k,l)
+sum_subterm_temp = sin(minus(theta_observed_by_ij_at_kl, theta_ij_extend));
+sum_subterm = W_4_dim.*sum_subterm_temp;
+% Perform sum over indices (k,l)
+summed_term = sum(sum_subterm,[3,4]);
+
+dthetadt_final = w + (K*1.0./N) .*summed_term;
+y_final = reshape(dthetadt_final, num_rows*num_cols,1);
+
+
--- main.py
+++ main.py
@@ -9,95 +9,101 @@ from os import path
 # Purpose: Reproduce results of Time-Delayed Spatial Patterns in a Two-Dimensional Array of Coupled Oscillators
 #           by Seong-Ok Jeong, Tae-Wook Ko, and Hie-Tae Moon of Korea Advanced Institute of Science and Technology.
 # Project for Phys 278
-# March 5, 2020
+# March 15, 2020
 #######################################################################
 ### Parameters and Initial Conditions
 #######################################################################
 sp.random.seed(2020)
-
-num_rows = 50  # 500 x 500 too big
-num_cols = 50  # 150 x 150 gives a 4 GB file
+Sparse = False;
+num_rows = 64  # 500 x 500 too big
+num_cols = 64  # 150 x 150 gives a 4 GB file
                 # 100 x 100 gives a 800 MB file
-
+use_this_param_set = '4c'
 grid_arr = sp.array([[[i,j] for j in range(num_cols)] for i in range(num_rows)])
 
-# k and i identify rows, l and j identify columns
-w = 7.0*sp.pi / 32 # used in paper
-K = 1.25 # varies, see fig 4
-v = 1.0/1
-Omega = 0.6*10
-gamma = 5 * sp.pi / 32 # one of values used in paper
-unit_vector_e = sp.array([1,1])
-phi_0 = sp.zeros((num_rows, num_cols))
-r_0 = 15 # coupling radius, experiment with this
-initial_conditions = sp.random.random((num_rows, num_cols)).reshape(num_rows*num_cols) # Flattens initial conditions
-numTimeSteps = 5
-t = sp.linspace(0,1,numTimeSteps)
+# The three most important tunable parameters:
 
-parameters = [num_rows, num_cols, w, K, v, Omega, gamma, unit_vector_e, r_0, initial_conditions,numTimeSteps, t]
+# order in list: [K, r_0, v]
+param_set = {'3a' : [1.25, 25, 1.0],
+             '3b' : [1.25, 25, 1.0],
+             '3c' : [1.25, 25, 1.0],
+             '3d' : [0.6, 10, 1.0/1.1],
+             '4a' : [1.0,  4, 1.0],
+             '4b' : [0.4,  7, 1.0],
+             '4c' : [0.8,  8, 1.0],
+             'NewA' : [0.2, 8, 1.0],
+             'NewB' : [0.2, 10, 1.0/0.8]
+            }
+K, r_0, v = param_set[use_this_param_set]
+
+# Further parameters:
+w = sp.pi/5.0 # used in paper. Do not change this.
+Omega = 0.6
+gamma = 5 * sp.pi / 32 # one of values used in paper
+unit_vector_e = sp.array([0,1])
+# phi_0 = 0
+
+# initial_conditions = sp.zeros((num_rows, num_cols)).reshape(num_rows*num_cols) # Flattens initial conditions
+# initial_conditions[num_rows*5+5]=sp.pi/2
+# initial_conditions = 2*sp.pi*sp.random.random((num_rows, num_cols)).reshape(num_rows*num_cols, order = 'F') # Flattens initial conditions
+initial_conditions = 0.000001*sp.random.random((num_rows, num_cols)).reshape(num_rows*num_cols, order = 'F') # Flattens initial conditions
+# initial_conditions = sp.array([[gamma*i for j in range(num_cols)] for i in range(num_rows)]).reshape(num_rows*num_cols, order = 'F') # Flattens initial conditions
+# initial_conditions = sp.array([[gamma*(i+j) for j in range(num_cols)] for i in range(num_rows)]).reshape(num_rows*num_cols, order = 'F') # Flattens initial conditions
+# initial_conditions = sp.array([[gamma*(i*j) for j in range(num_cols)] for i in range(num_rows)]).reshape(num_rows*num_cols, order = 'F') # Flattens initial conditions
+numTimeSteps = 10
+# t = sp.linspace(0,8,numTimeSteps)
+# phi = dfs.calc_phi(gamma, unit_vector_e, phi_0, grid_arr)
+# phi = sp.array([[gamma*i for j in range(num_cols)] for i in range(num_rows)])
+# phi = sp.random.random((num_rows,num_cols))
+
 
 #######################################################################
-### Calculate or load W matrix
+### Calculate W matrix
 #######################################################################
 W = sp.zeros((num_rows, num_cols, num_rows, num_cols))
 N = sp.empty((num_rows, num_cols))
-dist_grid_arr = -1*sp.ones((num_rows, num_cols, num_rows, num_cols)) # i, j, k, l
+dist_grid_arr = sp.zeros((num_rows, num_cols, num_rows, num_cols)) # i, j, k, l
 # Calculate W_klij for all ij and kl.
 start_time = time.time()
-
-# Make sure to delete old W matrix file if r_0 is changed.
-loaded_r_0 = r_0 # default value
-if path.exists("r_0.npz"):
-    print("Loading previous r_0")
-    loaded_r_0 = sp.load("r_0.npz")["r_0"]
-    if loaded_r_0 != r_0:
-        print("Using new r_0. Saving old r_0.")
-        sp.savez("r_0_previous.npz", r_0=loaded_r_0)
-    else:
-        print("Using current r_0 (same as previous r_0).")
+if Sparse:
+    W, dist_grid_arr = dfs.calc_W_and_dist_grid_sparse(num_rows, num_cols, r_0)
 else:
-    print("Previous r_0 not available. Using new r_0.")
-    sp.savez("r_0.npz", r_0=r_0)
-
-
-if path.exists("W_matrix_"+str((num_rows,num_cols,r_0))+".npz"): # If file exists
-    W_loaded = sp.load("W_matrix_"+str((num_rows,num_cols,r_0))+".npz")["W"]
-    if (sp.shape(W_loaded)[0]!=num_rows or sp.shape(W_loaded)[1]!=num_cols or r_0 != loaded_r_0): # if dimensions of loaded array not same as specified
-        print("Loaded W matrix doesn't meet specified dimensions. Saving old W file and making new one.")
-        sp.savez("W_matrix_previous.npz", W = W_loaded)
-        W, dist_grid_arr = dfs.calc_and_save_W_and_dist_grid(num_rows, num_cols, r_0)
-    if (sp.shape(W_loaded)[0]==num_rows and sp.shape(W_loaded)[1]==num_cols and (r_0 == loaded_r_0)):
-        # if dimensions of loaded array are same as specified
-        W = W_loaded
-        print("W matrix loaded.")
-else: # If file does not yet exist
-    W, dist_grid_arr = dfs.calc_and_save_W_and_dist_grid(num_rows, num_cols, r_0)
+    W, dist_grid_arr = dfs.calc_W_and_dist_grid(num_rows, num_cols, r_0)
+print("W and dist_grid_arr calculated.")
 
 #######################################################################
 ### Rest of the Calculations
 #######################################################################
-N = dfs.N(W, num_cols, num_rows)
-N = 1
-phi  = dfs.calc_phi(gamma, unit_vector_e, phi_0, grid_arr)
-print("Calculating solution for all selected times.")
-solution = odeint(dfs.theODEs, initial_conditions, t, args=(phi, w, K, N, v, Omega, num_rows, num_cols, W,
-                                                            dist_grid_arr))
-sp.savez("solution_("+str(num_cols)+","+str(num_rows)+")_r0_"+str(r_0), solution = solution, parameters = parameters, dist_grid_arr = dist_grid_arr)
-#######################################################################
-### Plot Solution
-#######################################################################
-print("Plotting solution")
-solution_2d = solution.reshape((numTimeSteps,num_rows,num_cols))
-print(sp.shape(solution))
-pyplt.title("Solution at Final Time")
-pyplt.imshow(solution_2d[numTimeSteps-1])
-pyplt.show()
-
-fig, ax = pyplt.subplots()
-num_video_loops = 10
-for j in range(num_video_loops):
-    for i in range(len(solution_2d)):
-        ax.cla()
-        ax.imshow(solution_2d[i], cmap = 'hsv', interpolation = 'nearest')
-        ax.set_title("Solution at frame {}".format(i))
-        pyplt.pause(0.1)
+N = dfs.calc_N(W)
+scalars = sp.array([w, K, v, Omega, gamma, num_rows, num_cols, numTimeSteps])
+dist_grid_arr_flattened = dist_grid_arr.reshape((num_rows*num_cols*num_rows*num_cols), order = 'F')
+lags_whole = dist_grid_arr/v # needed to reduce number of lags to make memory manageable.
+lags_reduced = sp.sort(sp.unique(dist_grid_arr_flattened/v))[1:] # needed to reduce number of lags to make memory manageable.
+lag_indices = sp.zeros((num_rows,num_cols,num_rows,num_cols))
+i=0
+l=0
+for j in range(num_cols):
+    for k in range(num_rows):
+        if sp.size(sp.where(lags_reduced == lags_whole[i,j,k,l])[0])!=0:
+            # print(sp.where(lags_reduced == lags_whole[i,j,k,l])[0][0])
+            lag_indices[i,j,k,l] = sp.where(lags_reduced == lags_whole[i,j,k,l])[0]+1
+for i in range(1,num_rows):
+    lag_indices[i, :, :, :] += sp.roll(lag_indices[0,:,:,:],i,axis=1)
+for l in range(1,num_cols):
+    lag_indices[:, :, :, l] += sp.roll(lag_indices[:,:,:,0],l,axis=1)
+lag_indices[lag_indices==0] = sp.nan
+# pyplt.figure()
+# pyplt.imshow(lags_whole[:,:,0,0])
+# pyplt.show()
+sp.save('generated_values/W.npy', W)
+sp.save('generated_values/dist_grid_arr.npy', dist_grid_arr)
+sp.save('generated_values/dist_grid_arr_flattened.npy', dist_grid_arr_flattened)
+sp.save('generated_values/lags_reduced.npy', lags_reduced)
+sp.save('generated_values/lags_whole.npy', lags_whole)
+sp.save('generated_values/lag_indices.npy', lag_indices)
+sp.save('generated_values/scalars.npy', scalars)
+# sp.save('generated_values/phi.npy', phi)
+sp.save('generated_values/N.npy', N)
+sp.save('generated_values/initial_conditions.npy',initial_conditions)
+print("Now run MATLAB code DDE.m to calculate solution for all selected times.")
+# Now run MATLAB code DDE.m
